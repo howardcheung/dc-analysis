8528aa9b54a3d5cb11bb54eb18970025ab3e6faf
Howard Cheung <tungeast@protonmail.com>
2017-03-28 16:56:42 +0800

    Initial commit
    Initial commit by adding the template python file to the repo.

---
+++ src/template.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+"""
+"""
+
+# import python internal modules
+import os
+
+# import python third party modules
+
+
+# import user-defined modules
+
+
+# define global variables
+
+
+# define methods
+
+
+# testing functions
+if __name__ == '__main__':
+
+    print('All functions in', os.path.basename(__file__), 'are ok')
