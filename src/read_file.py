2b4a81ed5710e15a02402e00507f42be2ad50b1d
Howard Cheung <tungeast@protonmail.com>
2017-03-28 17:28:36 +0800

    Extract name of server
    Extract name of a server from a file

--- src/read_file.py
+++ src/read_file.py
@@ -6,6 +6,7 @@
 
 # import python internal modules
 import os
+import re
 
 # import python third party modules
 
@@ -17,10 +18,10 @@ import os
 
 
 # define methods
-def read_file(filename: str):
+def read_file(filename: str) -> bytes:
     """
         This method reads the file specified by the user-given path. Return
-        everything in the file in a string.
+        everything in the file in a bytes object.
 
         Inputs:
         ==========
@@ -32,10 +33,36 @@ def read_file(filename: str):
         return openedfile.read()
 
 
+def extract_name(content: bytes) -> bytes:
+    """
+        This function extracts the name of the server being tested and return
+        a byte object. If it cannot find it, return a None object.
+
+        Inputs:
+        ==========
+        content: bytes
+            byte character obtained by reading a file
+    """
+
+    # define regular expression to be looked at
+    reg_exp = re.compile(b'Set Description:.*\n')
+    # identify where it is
+    try:
+        return reg_exp.search(content).group().replace(
+            b'Set Description:', b''
+        ).replace(b'\n', b'').strip()  # remove all redundant characters
+    except AttributeError:
+        return None
+
+
 # testing functions
 if __name__ == '__main__':
 
     # define testing function for read_file() method
     assert read_file('./read_file.py')[0:2] == b'#!'
+
+    # extract name for index
+    CONTENT = read_file('../data/power_ssj2008-20071128-00001.txt')
+    assert extract_name(CONTENT) == b'PRIMERGY RX300 S3 (Intel Xeon L5335)'
 
     print('All functions in', os.path.basename(__file__), 'are ok')
