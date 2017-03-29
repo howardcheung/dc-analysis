5c7cdf89cedb3e0fa40921d0752d34a461d882de
Howard Cheung <tungeast@protonmail.com>
2017-03-29 13:41:48 +0800

    Create a File for Database creation
    The data from the SPEC2008 should be organized in a pandas
    DataFrame for analysis. This script is created to use the
    methods in read_file.py to create the database.

---
+++ src/create_df.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+"""
+    This file contains methods to pass the data from multiple files to
+    a pandas DataFrame
+
+    Author: Howard Cheung
+    Date of creation: 2017/03/29
+"""
+
+# import python internal modules
+import os
+
+# import python third party modules
+
+
+# import user-defined modules
+
+
+# define global variables
+
+
+# define methods
+
+
+# testing functions
+if __name__ == '__main__':
+
+    print('All functions in', os.path.basename(__file__), 'are ok')
