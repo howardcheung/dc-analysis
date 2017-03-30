740014381c580b72c49b4bf5f6dbec034574136d
Howard Cheung <tungeast@protonmail.com>
2017-03-30 13:16:46 +0800

    Analyze the CPU speed and number of cores Effect
    Observe the change of power with CPU speed and number
    of cores as time goes

--- src/analysis.py
+++ src/analysis.py
@@ -8,6 +8,7 @@
 
 # import python internal modules
 import os
+import pdb
 
 # import python third party modules
 import pandas as pd
@@ -36,24 +37,33 @@ def summary_yr(datadf: pd.DataFrame) -> pd.DataFrame:
         datadf.loc[:, 'FormFac']
     datadf.loc[:, 'AdjMaxPower'] = datadf.loc[:, 'MaxPower'] /\
         datadf.loc[:, 'FormFac']
+    datadf.loc[:, 'AdjNumCores'] = datadf.loc[:, 'NumCores'] /\
+        datadf.loc[:, 'FormFac']
 
     # group the data
     grouped = datadf.groupby('Year')
     summary_df = pd.concat([
         grouped['AdjIdlePower'].mean(), grouped['AdjMaxPower'].mean(),
-        grouped['IdlePower'].count()
+        grouped['IdlePower'].count(), grouped['CPU speed'].mean(),
+        grouped['AdjNumCores'].mean()
     ], axis=1)
     summary_df.loc['All', :] = [
         datadf['AdjIdlePower'].mean(), datadf['AdjMaxPower'].mean(),
-        datadf['IdlePower'].count()
+        datadf['IdlePower'].count(), datadf['CPU speed'].mean(),
+        datadf['AdjNumCores'].mean()
     ]
-    summary_df.columns = ['AdjIdlePower', 'AdjMaxPower', 'Count']
+    summary_df.columns = [
+        'AdjIdlePower', 'AdjMaxPower', 'Count', 'AvCPUspd',
+        'NumCoresPer1U'
+    ]
 
     # calculate the required density values
     summary_df.loc[:, 'IdlePowerDens'] = \
         summary_df.loc[:, 'AdjIdlePower']*42.0/4.379*0.47
     summary_df.loc[:, 'MaxPowerDens'] = \
         summary_df.loc[:, 'AdjMaxPower']*42.0/4.379*0.47
+    summary_df.loc[:, 'NumCoresPerRack'] = \
+        summary_df.loc[:, 'NumCoresPer1U']*42.0*0.47
 
     return summary_df
 
